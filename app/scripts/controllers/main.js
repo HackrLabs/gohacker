98dc04698590963292b8afb29fd5cdce92342f48
Danny Grove <danny@drgrovellc.com>
2014-04-27 17:36:07 -0400

    Adds geocoding center and map refresh

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -2,29 +2,28 @@
 
 angular.module('gohackerApp')
   .controller('MainCtrl', function ($scope, $rootScope) {
-    $scope.position = 
-    { latitude: 35
-    , longitude: -100
-    }
+    $scope.position = {};
+    angular.extend($scope,
+      { map:
+        { control: {}
+        , center:
+          { latitude: $scope.position.latitude || 35
+          , longitude: $scope.position.longitude || -100
+          }
+        , options:
+          { maxZoom: 20
+          , minZoom: 3
+          }
+        , zoom: 4
+        , dragging: true
+        }
+      }
+    )
     navigator.geolocation.getCurrentPosition(function(position){
-    $scope.position = position.coords
-      $scope.position = position
+      $scope.position = position.coords
+      $scope.map.control.refresh($scope.position)
+      $scope.map.control.getGMap().setZoom(11)
     }, function(error){
       alert('Error occurred. Error code: ' + error.code)
     })
-  	/**
-  	 * The map objects
-  	 */
-  	$scope.map = {
-	    center: {
-	        latitude: $scope.position.latitude,
-	        longitude: $scope.position.longitude
-	    },
-	    zoom: 4,
-	    draggable: "true",
-	    options: false,
-	    bounds: false
-	};
-
-
   });
