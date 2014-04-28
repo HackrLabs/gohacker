7294d0a32961d39e7bd2bc9aa4c58223a6d3229b
AJ Seelund <aj@notdefault.com>
2014-04-27 23:27:16 -0400

    results from geocoder -- status check

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -32,21 +32,44 @@ angular.module('gohackerApp')
       alert('Error occurred. Error code: ' + error.code )
     })
 
+    /**
+     * Searches a geocoder results array for the first object designated a 'locality'
+     *
+     * @param  {Array} items Geocoder Results Array
+     * @return {String}       The locality i=items formatted_address field.
+     */
+    function getLocality( items ){
+      for (var i = items.length - 1; i >= 0; i--) {
+          if( items[i].types.indexOf('locality') > -1 ){
+            return items[i].formatted_address;
+          }
+      };
+    }
+
 
     $scope.goSearch = function(){
       var query = $scope.query;
       var gc = new google.maps.Geocoder();
       var gcRequest = {};
           gcRequest.location = new google.maps.LatLng($scope.position.latitude, $scope.position.longitude);
-          gcRequest.address = "Hackerspaces near "; // + $scope.position.latitude + ", " + $scope.position.longitude;
+
+          //This produces 0 results.
+          //gcRequest.address = "Hackerspaces";
 
       gc.geocode( gcRequest, function( ResultsArray, GeocoderStatus){
-        alert( ResultsArray);
+
+        if ( GeocoderStatus == "OK" ){
+          var locality = getLocality( ResultsArray );
+          alert( GeocoderStatus + "\nResults for: " + locality + "\n" + ResultsArray.length + " items.");
+        }else{
+          alert( GeocoderStatus + "\nNo Results found!");
+        }
 
       } );
 
 
       /*
+      //pseudo for API
       var searcher = new spaceSearch();
       var results = searcher.find( query );
       alert( results );
